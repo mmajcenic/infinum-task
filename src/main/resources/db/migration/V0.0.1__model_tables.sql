38df709ae606c67274866a06c8d01946967c9438
mmajcenic <marko.majcenic@gmail.com>
2020-07-30 00:00:43 +0200

    Separated initial cities creation from table creation in flyway migrations list

---
+++ src/main/resources/db/migration/V0.0.1__model_tables.sql
@@ -0,0 +1,28 @@
+-- User
+CREATE TABLE application_user (
+	id                                   BIGSERIAL PRIMARY KEY,
+	email                                TEXT NOT NULL UNIQUE,
+	password                             TEXT NOT NULL,
+	created_at                           TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_modified_at                     TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+-- City
+CREATE TABLE city (
+	id                                   BIGSERIAL PRIMARY KEY,
+	name                                 TEXT NOT NULL UNIQUE,
+	description                          TEXT NOT NULL,
+	population                           INTEGER NOT NULL,
+    favourite_count                      INTEGER DEFAULT 0,
+    created_at                           TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_modified_at                     TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE INDEX city_name_i ON city(name);
+
+-- Favourite user cities
+CREATE TABLE user_favourite_city (
+	application_user_id                  BIGINT REFERENCES application_user(id),
+	city_id                              BIGINT REFERENCES city(id),
+	PRIMARY KEY(application_user_id, city_id)
+);
--- src/main/resources/db/migration/V0.0.2__initial_cities.sql
+++ src/main/resources/db/migration/V0.0.2__initial_cities.sql
@@ -1,32 +1,3 @@
--- User
-CREATE TABLE application_user (
-	id                                   BIGSERIAL PRIMARY KEY,
-	email                                TEXT NOT NULL UNIQUE,
-	password                             TEXT NOT NULL,
-	created_at                           TIMESTAMP WITH TIME ZONE NOT NULL,
-    last_modified_at                     TIMESTAMP WITH TIME ZONE NOT NULL
-);
-
--- City
-CREATE TABLE city (
-	id                                   BIGSERIAL PRIMARY KEY,
-	name                                 TEXT NOT NULL UNIQUE,
-	description                          TEXT NOT NULL,
-	population                           INTEGER NOT NULL,
-    favourite_count                      INTEGER DEFAULT 0,
-    created_at                           TIMESTAMP WITH TIME ZONE NOT NULL,
-    last_modified_at                     TIMESTAMP WITH TIME ZONE NOT NULL
-);
-
-CREATE INDEX city_name_i ON city(name);
-
--- Favourite user cities
-CREATE TABLE user_favourite_city (
-	application_user_id                  BIGINT REFERENCES application_user(id),
-	city_id                              BIGINT REFERENCES city(id),
-	PRIMARY KEY(application_user_id, city_id)
-);
-
 INSERT INTO city(name, description, population, created_at, last_modified_at) VALUES('Zagreb', 'Capital city of Croatia', 100000, current_timestamp, current_timestamp);
 INSERT INTO city(name, description, population, created_at, last_modified_at) VALUES('Ljubljana', 'Capital city of Slovenia', 200000, current_timestamp, current_timestamp);
 INSERT INTO city(name, description, population, created_at, last_modified_at) VALUES('Rome', 'Capital city of Italy', 300000, current_timestamp, current_timestamp);
