45e9b1019c0ee90ff5104380f3fa2b129f3409d6
mmajcenic <marko.majcenic@gmail.com>
2020-07-29 17:35:25 +0200

    Bootstrapped project structure

---
+++ src/main/java/hr/infinum/task/infinum/InfinumTaskApplication.java
@@ -0,0 +1,13 @@
+package hr.infinum.task.infinum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinumTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinumTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/infinum/task/infinum/InfinumTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.infinum.task.infinum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinumTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
