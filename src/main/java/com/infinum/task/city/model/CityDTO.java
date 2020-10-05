9e9aa147a7cf384b6ce0a758b2983a1f0e464a8e
mmajcenic <marko.majcenic@gmail.com>
2020-10-06 00:00:13 +0200

    Added HATEOAS to city paging

--- src/main/java/com/infinum/task/city/model/CityDTO.java
+++ src/main/java/com/infinum/task/city/model/CityDTO.java
@@ -2,11 +2,14 @@ package com.infinum.task.city.model;
 
 import com.infinum.task.shared.PersistenceEntity;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.springframework.hateoas.RepresentationModel;
 
+@EqualsAndHashCode(callSuper = true)
 @Builder
 @Value
-public class CityDTO implements PersistenceEntity {
+public class CityDTO extends RepresentationModel<CityDTO> implements PersistenceEntity {
 
     Long id;
 
--- src/main/java/com/infinum/task/city/web/CityController.java
+++ src/main/java/com/infinum/task/city/web/CityController.java
@@ -10,6 +10,9 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +23,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Api(tags = "Cities")
 @RequiredArgsConstructor
 @RestController
@@ -28,6 +34,8 @@ public class CityController {
 
     private final CityServiceFacade cityServiceFacade;
 
+    private final PagedResourcesAssembler<CityDTO> pagedResourcesAssembler;
+
     @ApiOperation(value = "Create city", response = CityDTO.class)
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Constraint violation")
@@ -35,20 +43,22 @@ public class CityController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CityDTO create(@RequestBody @Valid final CreateCityRequest createCityRequest) {
-        return cityServiceFacade.create(createCityRequest);
+        final CityDTO cityDTO = cityServiceFacade.create(createCityRequest);
+        return cityDTO;
     }
 
     @ApiOperation(value = "Get all cities", response = CityDTO[].class)
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Page<CityDTO> getAllCities(@RequestParam(name = "sort_by_favourites",
+    public PagedModel<EntityModel<CityDTO>> getAllCities(@RequestParam(name = "sort_by_favourites",
             defaultValue = "false")
                                       @ApiParam(name = "sort_by_favourites",
                                               value = "Specify if cities should be sorted by favourites",
                                               defaultValue = "false") final boolean sortByFavourites,
-                                      @RequestParam("page") int page,
-                                      @RequestParam("size") int size) {
-        return cityServiceFacade.getAllCities(sortByFavourites, page, size);
+                                                        @RequestParam("page") int page,
+                                                        @RequestParam("size") int size) {
+        final Page<CityDTO> cityDTOPage = cityServiceFacade.getAllCities(sortByFavourites, page, size);
+        return pagedResourcesAssembler.toModel(cityDTOPage);
     }
 
 }
